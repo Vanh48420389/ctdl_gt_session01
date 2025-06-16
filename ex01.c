b321c4a66ecb324e30b8ceed214b6a41b63b0f1c
Vanh48420389 <sivanh740@gamil.com>
2025-06-16 09:48:38 +0700

    push code

--- ex01.c
+++ ex01.c
@@ -2,8 +2,12 @@
 // Created by Lenovo on 16/06/2025.
 //
 #include <stdio.h>
-int main()
-{
-    printf("hello world\n");
+int main(){
+    int n;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Hello\n");
+    }
     return 0;
 }
+// Kết quả độ phức tạp là O(n) tại vì
